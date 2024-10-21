84774c88d04b83726624b44feee937513bbe405f
juniorAsaki <jncho1717@gmail.com>
2024-10-21 12:05:01 +0000

    first commit

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageService} from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private localStorageService: LocalStorageService
+  ) { }
+
+
+  isAuthenticated(): boolean{
+    return !!this.localStorageService.getToken();
+  }
+
+
+}
